a05c841f359632bdf11563491e8ff057760d856a
kevin <ksgonzalez614@misena.edu.co>
2024-05-22 12:14:07 -0500

    guardar clase pago

--- src/main/java/Modelo/pago.java
+++ src/main/java/Modelo/pago.java
@@ -4,10 +4,20 @@
  */
 package Modelo;
 
+import java.util.Date;
+
 /**
  *
  * @author APRENDIZ
  */
 public class pago {
     
+    private String idPagos;
+    private int Referente;
+    private cotizante cotizante;
+    private Date fechaPago;
+    private int valor;
+    private evento evento;
+    private cliente cliente;
+       
 }
